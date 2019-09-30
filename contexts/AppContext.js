7ad4b771487b11baeeb6e6faf977fb85dde4e2e0
RockRonnie <45205523+RockRonnie@users.noreply.github.com>
2019-09-30 15:58:51 +0300

    updatingg

--- contexts/AppContext.js
+++ contexts/AppContext.js
@@ -4,22 +4,22 @@ import PropTypes from 'prop-types';
 const AppContext = React.createContext({});
 const AppProvider = (props) => {
   const {
-    article: initialArticle,
+    articles: initialArticle,
     user: initialUser,
-    myArticle: initialMyArticle,
+    myArticles: initialMyArticle,
     children,
   } = props;
-  const [article, setArticle] = useState(initialArticle);
+  const [articles, setArticles] = useState(initialArticle);
   const [user, setUser] = useState(initialUser);
-  const [myArticle, setMyArticle] = useState(initialMyArticle);
+  const [myArticles, setMyArticles] = useState(initialMyArticle);
 
   const applicationContext = {
     user,
     setUser,
-    article,
-    setArticle,
-    myArticle,
-    setMyArticle,
+    articles,
+    setArticles,
+    myArticles,
+    setMyArticles,
   };
 
   return (
@@ -30,15 +30,15 @@ const AppProvider = (props) => {
 };
 
 AppProvider.propTypes = {
-  media: PropTypes.array,
-  myMedia: PropTypes.array,
+  articles: PropTypes.array,
+  myArticles: PropTypes.array,
   user: PropTypes.object,
   children: PropTypes.node,
 };
 
 AppProvider.defaultProps = {
-  media: [],
-  myMedia: [],
+  articles: [],
+  myArticles: [],
   user: {},
 };
 
