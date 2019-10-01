1cacb10c799afae2322513309dea7d31add0cf81
Ilias Doukas <ilias.doukas@gmail.com>
2019-10-01 20:06:23 +0300

    File upload launch + creator button

--- views/Main.js
+++ views/Main.js
@@ -1,12 +1,16 @@
 /* eslint-disable max-len */
 import React from 'react';
-import {Container, Content, Text} from 'native-base';
+import {Container, Content, Text, Button} from 'native-base';
 
 const Main = (props) => {
   return (
     <Container>
       <Content>
         <Text>This is Main page</Text>
+        <Button onPress={() =>
+        {props.navigation.navigate('Creator')}}>
+          <Text>Creator</Text>
+        </Button>
       </Content>
     </Container>
   );
