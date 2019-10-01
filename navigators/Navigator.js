a1a8d3676583cf5c343ee8025edb99d113f30352
RockRonnie <45205523+RockRonnie@users.noreply.github.com>
2019-10-01 19:21:08 +0300

    navigation patch

--- navigators/Navigator.js
+++ navigators/Navigator.js
@@ -1,116 +1,104 @@
-import React, {useState, useEffect} from 'react';
-import {createAppContainer, createSwitchNavigator} from 'react-navigation';
-import {createBottomTabNavigator} from 'react-navigation-tabs';
-import {createStackNavigator} from 'react-navigation-stack';
-import GuestMain from '../views/GuestMain';
-import LoggedMain from '../views/LoggedMain';
-import User from '../views/User';
-import Article from '../views/Article';
-import Creator from '../views/Creator';
-import Auth from '../views/Auth';
-import Login from '../views/Login';
-import MyArticles from '../views/MyArticles';
-import {Icon} from 'native-base';
+import React, { useState, useEffect } from "react";
+import { createAppContainer, createSwitchNavigator } from "react-navigation";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+import { createStackNavigator } from "react-navigation-stack";
+import Main from "../views/Main";
+import User from "../views/User";
+import Article from "../views/Article";
+import Creator from "../views/Creator";
+import Auth from "../views/Auth";
+import Login from "../views/Login";
+import MyArticles from "../views/MyArticles";
+import { Icon } from "native-base";
 
 //NAVIGAATIO ei toimi ennen kun kaikkiin vieweihin on exportit
 
 const GuestTabNavigator = createBottomTabNavigator(
-    {
-      GuestMain,
-      Login,
-    },
-    {
-      defaultNavigationOptions: ({navigation}) => ({
-        tabBarIcon: () => {
-          const {routeName} = navigation.state;
-          let iconName;
-          if (routeName === 'GuestMain') {
-            iconName = 'home';
-          } else if (routeName === 'Login') {
-            iconName = 'person';
-          }
-          // You can return any component that you like here!
-          return <Icon
-            name={iconName}
-            size={25}
-          />;
-        },
-      }),
-    }
+  {
+    Main,
+    Login
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: () => {
+        const { routeName } = navigation.state;
+        let iconName;
+        if (routeName === "Main") {
+          iconName = "home";
+        } else if (routeName === "Login") {
+          iconName = "person";
+        }
+        // You can return any component that you like here!
+        return <Icon name={iconName} size={25} />;
+      }
+    })
+  }
 );
 const LoggedTabNavigator = createBottomTabNavigator(
-    {
-      LoggedMain,
-      Creator,
-      User,
-    },
-    {
-      defaultNavigationOptions: ({navigation}) => ({
-        tabBarIcon: () => {
-          const {routeName} = navigation.state;
-          let iconName;
-          if (routeName === 'Main') {
-            iconName = 'home';
-          } else if (routeName === 'User') {
-            iconName = 'person';
-          } else if (routeName === 'Creator') {
-            iconName = 'md-cloud-upload';
-          }
+  {
+    Main,
+    Creator,
+    User
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: () => {
+        const { routeName } = navigation.state;
+        let iconName;
+        if (routeName === "Main") {
+          iconName = "home";
+        } else if (routeName === "User") {
+          iconName = "person";
+        } else if (routeName === "Creator") {
+          iconName = "md-cloud-upload";
+        }
 
-          // You can return any component that you like here!
-          return <Icon
-            name={iconName}
-            size={25}
-          />;
-        },
-      }),
+        // You can return any component that you like here!
+        return <Icon name={iconName} size={25} />;
+      }
+    })
+  }
+);
+
+const LoggedStackNavigator = createStackNavigator({
+  Main: {
+    screen: LoggedTabNavigator,
+    navigationOptions: {
+      header: null // this will hide the header
     }
-);
+  },
+  Article: {
+    screen: Article
+  },
+  Logout: {
+    screen: Login
+  },
+  MyArticles: {
+    screen: MyArticles
+  }
+});
 
-const LoggedStackNavigator = createStackNavigator(
-    {
-      LoggedMain: {
-        screen: LoggedTabNavigator,
-        navigationOptions: {
-          header: null, // this will hide the header
-        },
-      },
-      Article: {
-        screen: Article,
-      },
-      Logout: {
-        screen: Login,
-      },
-      MyArticles: {
-        screen: MyArticles,
-      },
-    },
-);
-
-const GuestStackNavigator = createStackNavigator(
-    {
-      GuestMain: {
-        screen: GuestTabNavigator,
-        navigationOptions: {
-          header: null, // this will hide the header
-        },
-      },
-      Article: {
-        screen: Article,
-      },
-    },
-);
-
+const GuestStackNavigator = createStackNavigator({
+  Main: {
+    screen: GuestTabNavigator,
+    navigationOptions: {
+      header: null // this will hide the header
+    }
+  },
+  Article: {
+    screen: Article
+  }
+});
 
 const Navigator = createSwitchNavigator(
-    {
-      Auth: Auth,
-      User: LoggedStackNavigator,
-      Guest: GuestStackNavigator,
-    },
-    {
-      initialRouteName: 'Auth',
-    }
+  {
+    Auth: Auth,
+    User: LoggedStackNavigator,
+    Guest: GuestStackNavigator
+  },
+  {
+    initialRouteName: "Auth"
+  }
 );
 
 export default createAppContainer(Navigator);
