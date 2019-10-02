85edb90399cbda3884d1bbd81f756c2d4683dcb7
RockRonnie <45205523+RockRonnie@users.noreply.github.com>
2019-10-02 16:41:17 +0300

    tweaking

--- views/Login.js
+++ views/Login.js
@@ -3,21 +3,26 @@ import React from 'react';
 import { Container, Header, Tab, Tabs } from 'native-base';
 import LoginForm from '../components/LoginForm';
 import RegisterForm from '../components/RegisterForm';
-
+import PropTypes from 'prop-types';
+//Login has tabs for both login and register
 const Login = (props) => {
+  const {navigation} = props;
   return (
     <Container>
       <Header hasTabs />
       <Tabs>
         <Tab heading="Login">
-          <LoginForm {...props} />
+          <LoginForm navigation={navigation} />
         </Tab>
         <Tab heading="Register">
-          <RegisterForm {...props} />
+          <RegisterForm navigation={navigation} />
         </Tab>
       </Tabs>
     </Container>
   );
+};
+Login.propTypes = {
+  navigation: PropTypes.object,
 };
 
 export default Login;
