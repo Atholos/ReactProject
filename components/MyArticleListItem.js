ef301039d70e6e81e2d4df027c9451c06f7dbdcb
RockRonnie <45205523+RockRonnie@users.noreply.github.com>
2019-10-05 17:29:15 +0300

    Another

--- components/MyArticleListItem.js
+++ components/MyArticleListItem.js
@@ -1,7 +1,7 @@
 /* eslint-disable max-len */
-import React, {useContext, useState} from 'react';
+import React, { useContext, useState } from 'react';
 import PropTypes from 'prop-types';
-import {Alert} from 'react-native';
+import { Alert } from 'react-native';
 import {
   ListItem,
   Left,
@@ -16,42 +16,41 @@ import {
   Icon
 } from 'native-base';
 import ArticleHooks from '../hooks/ArticleHooks';
-import {MediaContext} from '../contexts/MediaContext';
+import { MediaContext } from '../contexts/MediaContext';
 import { AppContext } from '../contexts/AppContext';
 import { TouchableOpacity } from 'react-native-gesture-handler';
-import {Image } from 'react-native';
+import { Image } from 'react-native';
 
 const MyArticleListItem = (props) => {
   const [desc, setDesc] = useState({});
-  const {setArticle, setMyArticle} = useContext(AppContext);
-  const {navigation, singleMedia} = props;
-  const {getThumbnail, deleteMedia} = ArticleHooks();
+  const { setArticle, setMyArticle } = useContext(AppContext);
+  const { navigation, singleMedia } = props;
+  const { getThumbnail, deleteMedia } = ArticleHooks();
   const tn = getThumbnail(singleMedia.file_id);
   console.log('thumbnails', tn);
   return (
-    <ListItem thumbnail>
-      <Card style={{flex: 1}}>
-        <TouchableOpacity
-        onPress={() => {navigation.push("Article", {
+    <ListItem thumbnail
+      onPress={() => {
+        navigation.push("Article", {
           file: singleMedia,
           filedesc: desc.text,
-         });
-        }}>
-            <CardItem>
-              <Body>
-                <Image source={{ uri: 'http://media.mw.metropolia.fi/wbma/uploads/' + tn.w320 }} style={{height: 200, width: '100%', flex: 1}}/>
-              </Body>
-            </CardItem>
-            <CardItem>
-            <Body>
-                  <Text>{singleMedia.title}</Text>
-                  <Text note numberOfLines={3}>
-                  {desc.text}
-                  </Text>
-                </Body>
-            </CardItem>
-            </TouchableOpacity>
-          </Card>
+        });
+      }}>
+      <Card style={{ flex: 1 }}>
+        <CardItem>
+          <Body>
+            <Image source={{ uri: 'http://media.mw.metropolia.fi/wbma/uploads/' + tn.w320 }} style={{ height: 200, width: '100%', flex: 1 }} />
+          </Body>
+        </CardItem>
+        <CardItem>
+          <Body>
+            <Text>{singleMedia.title}</Text>
+            <Text note numberOfLines={3}>
+              {desc.text}
+            </Text>
+          </Body>
+        </CardItem>
+      </Card>
     </ListItem>
   );
 };
