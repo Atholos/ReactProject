fb50f9a159c7b9287af0369f36c7c2a5ff7941e7
Ilias Doukas <ilias.doukas@gmail.com>
2019-10-05 18:49:38 +0300

    userpage ver2 + avatar näkyy

--- hooks/ArticleHooks.js
+++ hooks/ArticleHooks.js
@@ -1,147 +1,147 @@
-import {useEffect, useContext, useState} from "react";
-import { AsyncStorage } from "react-native";
+import {useEffect, useContext, useState} from 'react';
+import {AsyncStorage} from 'react-native';
 import {AppContext} from '../contexts/AppContext';
 import {MediaContext} from '../contexts/MediaContext';
 
 const apiUrl = 'http://media.mw.metropolia.fi/wbma/';
 
-  const fetchGetUrl = async (url) => {
-    // PLACEHOLDER token. TEE KUNNOLLA !!!!! !!!    !
-    const userToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxMTksInVzZXJuYW1lIjoiYXNkIiwiZW1haWwiOiJlYmluMTIzQGhvdG1haWwuY29tIiwiZnVsbF9uYW1lIjpudWxsLCJpc19hZG1pbiI6bnVsbCwidGltZV9jcmVhdGVkIjoiMjAxOS0wMS0yNFQxMDoyMzoyOC4wMDBaIiwiaWF0IjoxNTY5NzQ1NzgwLCJleHAiOjE1NzE4MTkzODB9.PN1qLUlFcQGK8Uqf3QMwDNtxFDRZegzVjfRIKsSbEVk';
-    const response = await fetch(url, {
-      headers: {
-        'x-access-token': userToken,
-      },
-    });
-    const json = await response.json();
-    //console.log('fetchUrl json', json);
-    return json;
-  };
+const fetchGetUrl = async (url) => {
+  // PLACEHOLDER token. TEE KUNNOLLA !!!!! !!!    !
+  const userToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxMTksInVzZXJuYW1lIjoiYXNkIiwiZW1haWwiOiJlYmluMTIzQGhvdG1haWwuY29tIiwiZnVsbF9uYW1lIjpudWxsLCJpc19hZG1pbiI6bnVsbCwidGltZV9jcmVhdGVkIjoiMjAxOS0wMS0yNFQxMDoyMzoyOC4wMDBaIiwiaWF0IjoxNTY5NzQ1NzgwLCJleHAiOjE1NzE4MTkzODB9.PN1qLUlFcQGK8Uqf3QMwDNtxFDRZegzVjfRIKsSbEVk';
+  const response = await fetch(url, {
+    headers: {
+      'x-access-token': userToken,
+    },
+  });
+  const json = await response.json();
+  // console.log('fetchUrl json', json);
+  return json;
+};
 
-  const fetchURL = async (url) => {
-    const userToken = await AsyncStorage.getItem('userToken');
-    console.log('fetchGetUrl', url);
-    const response = await fetch(url, {
-      headers: {
-        'x-access-token': userToken,
-      },
-    });
-    const json = await response.json();
-    console.log('fetchUrl json', json);
-    return json;
-  };
+const fetchURL = async (url) => {
+  const userToken = await AsyncStorage.getItem('userToken');
+  console.log('fetchGetUrl', url);
+  const response = await fetch(url, {
+    headers: {
+      'x-access-token': userToken,
+    },
+  });
+  const json = await response.json();
+  console.log('fetchUrl json', json);
+  return json;
+};
 
-  const getTagFiles = async () => {
-    console.log('TAGISSA ON KÄYTY')
-    const tagresult = await fetchGetUrl(apiUrl + 'tags/craftersguild')
-    return tagresult;
-  }
+const getTagFiles = async () => {
+  console.log('TAGISSA ON KÄYTY');
+  const tagresult = await fetchGetUrl(apiUrl + 'tags/craftersguild');
+  return tagresult;
+};
 
 const ArticleHooks = () => {
 
   const getArticleDesc = async (fileid) => {
-    const descResult = await fetchGetUrl(apiUrl + 'tags/file/' + fileid)
+    const descResult = await fetchGetUrl(apiUrl + 'tags/file/' + fileid);
     console.log(descResult);
     for (let i=0; i < descResult.length; i++) {
       if (descResult[i].tag.length > 30) {
         return descResult[i].tag;
-        console.log('JACKPOT', descResult[i].tag)
+        console.log('JACKPOT', descResult[i].tag);
       }
     }
-    return "Description not found"
-  }
+    return 'Description not found';
+  };
 
-    const getAllMedia = () => {
-      console.log('getAllMedia')
-        const [articles, setArticles] = useContext(AppContext);
-        const [loading, setLoading] = useState(true);
-        useEffect(() => {
-          fetchGetUrl(apiUrl + 'media').then((json) => {
-            setArticles(json);
-            setLoading(false);
-          });
-        }, []);
-        return [articles, loading];
-      };
+  const getAllMedia = () => {
+    console.log('getAllMedia');
+    const [articles, setArticles] = useContext(AppContext);
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+      fetchGetUrl(apiUrl + 'media').then((json) => {
+        setArticles(json);
+        setLoading(false);
+      });
+    }, []);
+    return [articles, loading];
+  };
 
-      const getThumbnail = (url) => {
-        const [thumbnails, setThumbnails] = useState({});
-        useEffect(() => {
-          fetchGetUrl(apiUrl + 'media/' + url).then((json) => {
-            setThumbnails(json.thumbnails);
-          });
-        }, []);
-        return thumbnails;
-      };
+  const getThumbnail = (url) => {
+    const [thumbnails, setThumbnails] = useState({});
+    useEffect(() => {
+      fetchGetUrl(apiUrl + 'media/' + url).then((json) => {
+        setThumbnails(json.thumbnails);
+      });
+    }, []);
+    return thumbnails;
+  };
 
-      const useFetch = (url) => {
-        const { articles, setArticles } = useContext(AppContext);
-        const [loading, setLoading] = useState(true);
-        const fetchUrl = async () => {
-          const tagfiles = await getTagFiles()
-          console.log('TAGFILES', tagfiles[1])
-          const tagFileId = [];
-          const taggedFilesList = [];
-          for (i=0;i < tagfiles.length; i++) {
-            tagFileId.push(tagfiles[i].file_id);
-          }
-          //console.log('tagFileID', tagFileId[1])
-          for (let i=0; i < tagFileId.length; i++) {
-            console.log('rullaa')
-          const response = await fetch(url + tagFileId[i]);
-          const json = await response.json();
-          taggedFilesList.push(json);
-        }
-          console.log('TAGGED FILES LIST', taggedFilesList);
-          setArticles(taggedFilesList);
-          setLoading(false);
-        };
-        useEffect(() => {
-          fetchUrl();
-        }, []);
-        return [articles, loading];
-      };
+  const useFetch = (url) => {
+    const {articles, setArticles} = useContext(AppContext);
+    const [loading, setLoading] = useState(true);
+    const fetchUrl = async () => {
+      const tagfiles = await getTagFiles();
+      console.log('TAGFILES', tagfiles[1]);
+      const tagFileId = [];
+      const taggedFilesList = [];
+      for (i=0; i < tagfiles.length; i++) {
+        tagFileId.push(tagfiles[i].file_id);
+      }
+      // console.log('tagFileID', tagFileId[1])
+      for (let i=0; i < tagFileId.length; i++) {
+        console.log('rullaa');
+        const response = await fetch(url + tagFileId[i]);
+        const json = await response.json();
+        taggedFilesList.push(json);
+      }
+      console.log('TAGGED FILES LIST', taggedFilesList);
+      setArticles(taggedFilesList);
+      setLoading(false);
+    };
+    useEffect(() => {
+      fetchUrl();
+    }, []);
+    return [articles, loading];
+  };
 
-      const getAllMyArticles = () => {
-        const { myArticles, setMyArticles } = useContext(AppContext);
-        const [loading, setLoading] = useState(true);
-        useEffect(() => {
-          fetchURL(apiUrl + 'media/user').then((json) => {
-            setMyArticles(json);
-            setLoading(false);
-          });
-        }, []);
-        return [myArticles, loading];
-      };
+  const getAllMyArticles = () => {
+    const {myArticles, setMyArticles} = useContext(AppContext);
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+      fetchURL(apiUrl + 'media/user').then((json) => {
+        setMyArticles(json);
+        setLoading(false);
+      });
+    }, []);
+    return [myArticles, loading];
+  };
 
-      const deleteArticle = async (file, setMyArticle, setArticle, navigation) => {
-        return fetchDeleteUrl('media/' + file.file_id).then((json) => {
-          console.log('delete', json);
-          setArticle([]);
-          setMyArticle([]);
-          setTimeout(() => {
-            // reloadAllMedia(setArticle, setMyArticle);
-            navigation.navigate('Profile');
-            Alert.alert(
-              'File Deleted',
-              'Reloading user files',
-              [
-                { text: 'OK', onPress: () => navigation.navigate('MyFiles') },
-              ],
-              { cancelable: false },
-            );
-          }, 2000);
-        });
-      };
+  const deleteArticle = async (file, setMyArticle, setArticle, navigation) => {
+    return fetchDeleteUrl('media/' + file.file_id).then((json) => {
+      console.log('delete', json);
+      setArticle([]);
+      setMyArticle([]);
+      setTimeout(() => {
+        // reloadAllMedia(setArticle, setMyArticle);
+        navigation.navigate('Profile');
+        Alert.alert(
+            'File Deleted',
+            'Reloading user files',
+            [
+              {text: 'OK', onPress: () => navigation.navigate('MyFiles')},
+            ],
+            {cancelable: false},
+        );
+      }, 2000);
+    });
+  };
 
-      return {
-          getAllMedia,
-          getThumbnail,
-          useFetch,
-          getArticleDesc,
-          getAllMyArticles,
-          deleteArticle,
-      };
-}
+  return {
+    getAllMedia,
+    getThumbnail,
+    useFetch,
+    getArticleDesc,
+    getAllMyArticles,
+    deleteArticle,
+  };
+};
 
 export default ArticleHooks;
--- views/User.js
+++ views/User.js
@@ -1,41 +1,53 @@
 /* eslint-disable max-len */
 import React, {useState, useEffect} from 'react';
 import {StyleSheet} from 'react-native';
-import { Container, Content, Text, Button } from 'native-base';
-import appHooks from '../hooks/MainHooks'
+import {Container, Content, Text, Button, Thumbnail} from 'native-base';
+import appHooks from '../hooks/MainHooks';
 
 const User = (props) => {
   const {
-        signOut,
-        getUser
-      } = appHooks();
+    signOut,
+    getUser,
+  } = appHooks();
 
   const [uinfo, setUinfo] = useState({});
 
   useEffect(() => {
     getUser().then((json) => {
-      console.log('USER DATA IN USER.JS', json)
-      const parsedJson = JSON.parse(json)
-        setUinfo(
+      console.log('USER DATA IN USER.JS', json);
+      const parsedJson = JSON.parse(json);
+      const date = parsedJson.time_created.split('T');
+      setUinfo(
           {
-          name: parsedJson.username,
-          email: parsedJson.email,
-          doc: parsedJson.time_created
-        }
-        );
-      }).catch((error) => {
-        console.log(error);
-      });
-    }, []);
+            name: parsedJson.username,
+            email: parsedJson.email,
+            doc: date[0],
+            id: parsedJson.user_id,
+          }
+      );
+    }).catch((error) => {
+      console.log(error);
+    });
+  }, []);
 
 
   return (
     <Container style={styles.container}>
       <Content>
         <Text style={styles.title}>Profile</Text>
+        <Thumbnail
+          square
+          large
+          source={{uri: 'http://media.mw.metropolia.fi/wbma/tags/Avatar' + uinfo.id}} />
         <Text>Welcome {uinfo.name}</Text>
         <Text>Email {uinfo.email}</Text>
         <Text>Member since {uinfo.doc}</Text>
+        <Button onPress={() => signOut(props)}>
+          <Text>Change password</Text>
+        </Button>
+        <Button onPress={() => signOut(props)}>
+          <Text>Change email</Text>
+        </Button>
         <Button onPress={() => signOut(props)}>
           <Text>Logout!</Text>
         </Button>
@@ -43,18 +55,18 @@ const User = (props) => {
     </Container>
   );
 };
-  const styles = StyleSheet.create({
-    container: {
-      alignItems: 'center',
-      justifyContent: 'center',
-      paddingTop: 40,
-    },
-    title: {
-      fontSize: 22,
-      fontWeight: '500',
-      marginBottom: 20,
-    },
-  });
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 40,
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: '500',
+    marginBottom: 20,
+  },
+});
 
 
 export default User;
