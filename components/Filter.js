faf8557ef79b45e0356ee166ad985d8bd8cda7cf
RockRonnie <45205523+RockRonnie@users.noreply.github.com>
2019-10-06 20:37:00 +0300

    beep

--- components/Filter.js
+++ components/Filter.js
@@ -1,27 +1,40 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Container, Header, Content, ListItem, CheckBox, Text, Body } from 'native-base';
 import appContext from '../contexts/AppContext.js'
 
+
 const Filter = () => {
+  [articles, setArticles] = useContext(appContext);
+  const cathegories = useState([]);
+
+
+  const addCategory = (cat, catlist) => {
+    const addCat = catlist.push(cat);
+
+  };
+  const removeCategory = (cat, catlist) => {
+    const removeCat = catlist.filter()
+
+  };
   return (
     <Container>
       <Header />
       <Content>
         <List>
           <ListItem>
-            <CheckBox checked={true} />
+            <CheckBox checked={false} value={arduino} />
             <Body>
               <Text>Daily Stand Up</Text>
             </Body>
           </ListItem>
           <ListItem>
-            <CheckBox checked={false} />
+            <CheckBox checked={false} value={automation} />
             <Body>
               <Text>Discussion with Client</Text>
             </Body>
           </ListItem>
           <ListItem>
-            <CheckBox checked={false} color="green" />
+            <CheckBox checked={false} value={electronics} color="green" />
             <Body>
               <Text>Finish list Screen</Text>
             </Body>
