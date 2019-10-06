97a7d38792466c9ef707ded344487ebe950bea63
RockRonnie <45205523+RockRonnie@users.noreply.github.com>
2019-10-06 13:00:29 +0300

    changes

--- components/RegisterForm.js
+++ components/RegisterForm.js
@@ -28,8 +28,8 @@ const RegisterForm = (props) => {
     handleEmailChange,
     handleFullnameChange,
   } = useLogRegForm();
-  const [image, setImage] = useState({selected: dummyURL});
- // const craftAvatar = 'https://www.catholicnewsagency.com/images/Beer_Credit_Africa_Studio_Shutterstock_CNA.jpg?w=760';
+  const craftAvatar = 'https://www.catholicnewsagency.com/images/Beer_Credit_Africa_Studio_Shutterstock_CNA.jpg?w=760';
+  const [image, setImage] = useState({selected: craftAvatar});
   const pickImage = async () => {
     const result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All,
