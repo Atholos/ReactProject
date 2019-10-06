34cd7bef98fc0ed45a1e13619bd674b59c086982
RockRonnie <45205523+RockRonnie@users.noreply.github.com>
2019-10-06 17:13:08 +0300

    Wee!

--- components/Filter.js
+++ components/Filter.js
@@ -2,28 +2,33 @@ import React from 'react';
 import { Container, Header, Content, ListItem, CheckBox, Text, Body } from 'native-base';
 import appContext from '../contexts/AppContext.js'
 
-return (
-  <Container>
-    <Header />
-    <Content>
-      <ListItem>
-        <CheckBox checked={true} />
-        <Body>
-          <Text>Daily Stand Up</Text>
-        </Body>
-      </ListItem>
-      <ListItem>
-        <CheckBox checked={false} />
-        <Body>
-          <Text>Discussion with Client</Text>
-        </Body>
-      </ListItem>
-      <ListItem>
-        <CheckBox checked={false} color="green"/>
-        <Body>
-          <Text>Finish list Screen</Text>
-        </Body>
-      </ListItem>
-    </Content>
-  </Container>
-);
+const Filter = () => {
+  return (
+    <Container>
+      <Header />
+      <Content>
+        <List>
+          <ListItem>
+            <CheckBox checked={true} />
+            <Body>
+              <Text>Daily Stand Up</Text>
+            </Body>
+          </ListItem>
+          <ListItem>
+            <CheckBox checked={false} />
+            <Body>
+              <Text>Discussion with Client</Text>
+            </Body>
+          </ListItem>
+          <ListItem>
+            <CheckBox checked={false} color="green" />
+            <Body>
+              <Text>Finish list Screen</Text>
+            </Body>
+          </ListItem>
+        </List>
+      </Content>
+    </Container>
+  );
+}
+export default Filter;
