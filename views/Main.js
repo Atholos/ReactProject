fbc02fe263f388916ee5c1ce8c74494f164579b4
RockRonnie <45205523+RockRonnie@users.noreply.github.com>
2019-10-07 15:19:00 +0300

    tryharding

--- views/Main.js
+++ views/Main.js
@@ -1,14 +1,14 @@
 /* eslint-disable max-len */
 import React from 'react';
-import {Container, Content, Header} from 'native-base';
+import { Container, Content, Header } from 'native-base';
 import { StatusBar } from 'react-native';
 import ArticleList from '../components/ArticleList';
 import PropTypes from 'prop-types';
+import Filter from '../components/Filter'
 
 const Main = (props) => {
-  const {navigation} = props;
+  const { navigation } = props;
   return (
-
     <Container>
       <Content>
         <ArticleList navigation={navigation}></ArticleList>
