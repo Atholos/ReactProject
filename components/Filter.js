9d5e9076e5f5ad570c35d6101ef00d1cc537b846
RockRonnie <45205523+RockRonnie@users.noreply.github.com>
2019-10-08 21:46:45 +0300

    Still trying to refresh

--- components/Filter.js
+++ components/Filter.js
@@ -2,22 +2,31 @@ import React, { useState, useContext } from 'react';
 import { Header, Left, Right, Body, Item, Button, Icon, Input, Text } from 'native-base'
 import { Image, ImageBackground } from 'react-native'
 import useSearch from '../hooks/SearchHooks';
+import { AppContext } from '../contexts/AppContext'
 
 const Filter = () => {
-
-  const {handleSearch, searchFilterFunction, search} = useSearch();
+  const { articles, setArticles } = useContext(AppContext);
+  const { handleSearch, searchFilterFunction, search } = useSearch();
 
   return (
     <Header searchBar rounded>
       <Item>
         <Icon name="ios-search" />
-        <Input placeholder="Search" onChangeText={text => handleSearch(text)} value={search.params} />
+        <Input
+          placeholder="Search"
+          onChangeText={text => handleSearch(text)}
+          value={search.params}
+          onEndEditing={() => {
+            reloadAllArticles().then((json) => {
+              setArticles(json);
+            });
+          }} />
         <Icon name="ios-people" />
       </Item>
       <Item>
-      <Button transparent onPress={() => searchFilterFunction()}>
-        <Text>Search</Text>
-      </Button>
+        <Button transparent small rounded onPress={() => searchFilterFunction(articles, setArticles)}>
+          <Text>Search</Text>
+        </Button>
       </Item>
     </Header>
   );
--- hooks/SearchHooks.js
+++ hooks/SearchHooks.js
@@ -1,12 +1,12 @@
-import { useState, useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import ArticleHooks from '../hooks/ArticleHooks';
-import { AppContext } from '../contexts/AppContext'
+import { Alert } from 'react-native'
+
 
 
 const useSearch = () => {
   const [search, setSearch] = useState({});
   const { reloadAllArticles } = ArticleHooks();
-  const { articles, setArticles } = useContext(AppContext);
 
   const handleSearch = (text) => {
     setSearch((search) =>
@@ -15,19 +15,17 @@ const useSearch = () => {
         params: text,
       }));
   };
-  const searchFilterFunction = () => {
+  const searchFilterFunction = (articles, setArticles) => {
     const setNewData = () => {
       const newData = articles.filter(item => {
         const itemData = `${item.title.toUpperCase()} ${item.description.toUpperCase()}`;
         const textData = search.params.toUpperCase();
         return itemData.indexOf(textData) > -1;
       });
-      setArticles(newData);
-    }
-    setArticles([]);
-    reloadAllArticles().then((json) => {
-      setArticles(json);
-    }).then(()=> {setNewData()});
+     setArticles(newData);
+
+    };
+    setNewData();
   };
   return {
     searchFilterFunction,
