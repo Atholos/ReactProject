176a6b5e10dec7aae1e79581a37e96df139a78ed
RockRonnie <45205523+RockRonnie@users.noreply.github.com>
2019-10-10 20:31:44 +0300

    add

--- components/MyArticleEdit.js
+++ components/MyArticleEdit.js
@@ -1,9 +1,10 @@
-import React, {useEffect, useState, useContext} from 'react';
-import {StyleSheet, Image, Alert} from 'react-native';
-import {Container, Content, Button, Text, Header, Tab, Tabs, Input, Thumbnail} from 'native-base';
+import React, { useEffect, useState, useContext } from 'react';
+import { StyleSheet, Image, Alert } from 'react-native';
+import { Container, Content, Button, Text, Header, Tab, Tabs, Input, Thumbnail, Icon, Form, Item, Textarea, Label } from 'native-base';
+import { Col, Row, Grid } from 'react-native-easy-grid';
 import appHooks from '../hooks/MainHooks';
 import ArticleHooks from '../hooks/ArticleHooks';
-import {AppContext} from '../contexts/AppContext';
+import { AppContext } from '../contexts/AppContext';
 import CommentList from '../components/CommentList';
 import CommentForm from '../components/CommentForm';
 import PropTypes from 'prop-types';
@@ -13,16 +14,16 @@ import appValidation from '../hooks/ValidationHooks';
 
 
 const MyArticleEdit = (props) => {
-  const {setArticles, setMyArticles, setAllArticles } = useContext(AppContext);
-  const {checkUser, getPermissionAsync} = appHooks();
-  const {navigation} = props;
-  const {deleteArticle} = ArticleHooks();
+  const { setArticles, setMyArticles, setAllArticles } = useContext(AppContext);
+  const { checkUser, getPermissionAsync } = appHooks();
+  const { navigation } = props;
+  const { deleteArticle } = ArticleHooks();
   const media = navigation.getParam('file', 'WRONG');
   const mediaDesc = navigation.getParam('filedesc', 'WRONG');
   const title = media.title;
   const fileID = media.file_id;
   const [uname, setUname] = useState({});
-  const [image, setImage] = useState({selected: 'http://media.mw.metropolia.fi/wbma/uploads/' + media.filename});
+  const [image, setImage] = useState({ selected: 'http://media.mw.metropolia.fi/wbma/uploads/' + media.filename });
   const {
     handleTitleChange,
     handleBodyChange,
@@ -31,19 +32,6 @@ const MyArticleEdit = (props) => {
   } = useUpdateForm();
   const { updateValidate } = appValidation();
 
-  const pickImage = async () => {
-    const result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
-      allowsEditing: true,
-      aspect: [4, 3],
-    });
-    console.log(result);
-    setImage(
-      {
-        selected: result.uri,
-      });
-  };
-
   useEffect(() => {
     getPermissionAsync();
   }
@@ -51,114 +39,110 @@ const MyArticleEdit = (props) => {
   useEffect(() => {
     console.log('Articlemedia!!!', media);
     checkUser(props).then((json) => {
-      setUname({name: json});
+      setUname({ name: json });
     }).catch((error) => {
-      console.log(error);x
+      console.log(error); x
     });
   }, []);
-    return (
-      <Content>
-        <Input 
-        autoCapitalize='none'
-        placeholder={title}
-        onChangeText={handleTitleChange}
-        value={update.title}
-        style={styles.title}>
-        </Input>
-        <Text style ={styles.desc}>{mediaDesc}</Text>
-        <Input 
-        autoCapitalize='none'
-        placeholder={media.description}
-        onChangeText={handleBodyChange}
-        value={update.body}
-        style ={styles.bodytext}>
-        </Input>
-        <Button
-          onPress={
-            () => {
-              console.log('press');
-  
-              Alert.alert(
-                  'EDIT',
-                  'You are updating this Article, press "OK" to proceed or "Cancel" to retract.',
-                  [
-                    {
-                      text: 'OK',
-                      onPress: () => {
-                        console.log('OK Pressed'),
-                        updateValidate(image.selected, fileID, update, navigation, setAllArticles, setArticles, setMyArticles);
-                      },
-                    },
-                    {text: 'Cancel',
-                      onPress: () => console.log('Cancel Pressed'),
-                      style: 'cancel'},
-                  ],
-                  {cancelable: false},
-              );
-            }
-          }
-        >
-          <Text>Edit</Text>
-        </Button>
-        <Button
-          onPress={
-            () => {
-              console.log('press');
-  
-              Alert.alert(
-                  'DELETE',
-                  'You are deleting this file for good, press "OK" to proceed or "Cancel" to retract.',
-                  [
-                    {
-                      text: 'OK',
-                      onPress: () => {
-                        console.log('OK Pressed'),
-                        deleteArticle(media, setMyArticles, setArticles, navigation);
-                      },
-                    },
-                    {text: 'Cancel',
-                      onPress: () => console.log('Cancel Pressed'),
-                      style: 'cancel'},
-                  ],
-                  {cancelable: false},
-              );
-            }
-          }
-        >
-          <Text>Delete</Text>
-        </Button>
-        </Content>
-    )};
-    
-    MyArticleEdit.propTypes = {
-        navigation: PropTypes.object,
-      };
-  
-  
-  const styles = StyleSheet.create({
-    container: {
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-      paddingTop: 40,
-    },
-    image: {
-      borderRadius: 16,
-      height: 250,
-      width: '100%',
-      marginBottom: 10,
-    },
-    title: {
-      fontSize: 32,
-      fontWeight: '600',
-      marginBottom: 30,
-    },
-    desc: {
-      marginBottom: 30,
-      fontWeight: '500',
-    },
-    bodytext: {
-    },
-  });
+  return (
+    <Content padder>
+      <Form>
+        <Grid>
+          <Col>
+            <Item>
+              {image.selected && <Thumbnail
+                source={{ uri: image.selected }} style={{ width: '100%', height: 200, alignSelf: 'center' }} />}
+            </Item>
+            <Item last rounded style={{ margin: 10 }}>
+              <Input
+                autoCapitalize='none'
+                placeholder={title}
+                onChangeText={handleTitleChange}
+                value={update.title}
+              />
+            </Item>
+            <Label>Article</Label>
+            <Textarea rowSpan={10} style={{ margin: 10 }}
+              bordered
+              rounded
+              autoCapitalize='none'
+              placeholder={media.description}
+              onChangeText={handleBodyChange}
+              value={update.body}
+               />
 
-  export default MyArticleEdit;
+            <Row style={{ height: 40 }}>
+              <Col>
+                <Button style={{ margin: 10 }} iconLeft small rounded onPress={
+                  () => {
+                    console.log('press');
+
+                    Alert.alert(
+                      'DELETE',
+                      'You are deleting this file for good, press "OK" to proceed or "Cancel" to retract.',
+                      [
+                        {
+                          text: 'OK',
+                          onPress: () => {
+                            console.log('OK Pressed'),
+                              deleteArticle(media, setMyArticles, setArticles, navigation);
+                          },
+                        },
+                        {
+                          text: 'Cancel',
+                          onPress: () => console.log('Cancel Pressed'),
+                          style: 'cancel'
+                        },
+                      ],
+                      { cancelable: false },
+                    );
+                  }
+                }>
+                  <Icon name='ios-nuclear' />
+                  <Text>DELETE</Text>
+                </Button>
+              </Col>
+              <Col>
+                <Button style={{ margin: 10 }} iconLeft small rounded
+                  onPress={
+                    () => {
+                      console.log('press');
+
+                      Alert.alert(
+                        'EDIT',
+                        'You are updating this Article, press "OK" to proceed or "Cancel" to retract.',
+                        [
+                          {
+                            text: 'OK',
+                            onPress: () => {
+                              console.log('OK Pressed'),
+                                updateValidate(image.selected, fileID, update, navigation, setAllArticles, setArticles, setMyArticles);
+                            },
+                          },
+                          {
+                            text: 'Cancel',
+                            onPress: () => console.log('Cancel Pressed'),
+                            style: 'cancel'
+                          },
+                        ],
+                        { cancelable: false },
+                      );
+                    }
+                  }
+                >
+                  <Icon name='md-refresh-circle'/>
+                  <Text>UPDATE</Text>
+                </Button>
+              </Col>
+            </Row>
+          </Col>
+        </Grid>
+      </Form>
+    </Content>
+  )
+};
+
+MyArticleEdit.propTypes = {
+  navigation: PropTypes.object,
+};
+export default MyArticleEdit;
