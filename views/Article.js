b0cedffe8d7346383d0a5ff948fd664982f11c2d
Ilias Doukas <ilias.doukas@gmail.com>
2019-10-10 16:32:42 +0300

    setImage nollaus pois

--- views/Article.js
+++ views/Article.js
@@ -59,7 +59,7 @@ const styles = StyleSheet.create({
     textAlign: 'center',
   },
   desc: {
-    marginBottom: 30,
+    marginBottom: 12,
     fontWeight: '500',
     marginLeft: 15,
     marginRight: 15,
