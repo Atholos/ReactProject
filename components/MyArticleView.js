56c5551b788b331e4951bf1e0fbce036d3be4ebd
Ilias Doukas <ilias.doukas@gmail.com>
2019-10-10 21:02:46 +0300

    articlelist

--- components/MyArticleView.js
+++ components/MyArticleView.js
@@ -1,14 +1,15 @@
 import React, {useEffect, useState, useContext} from 'react';
 import {Platform, StyleSheet, Image, Alert} from 'react-native';
-import {Container, Content, Button, Text, Header, Tab, Tabs} from 'native-base';
+import {Card, CardItem, Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text} from 'native-base';
 import appHooks from '../hooks/MainHooks';
 import ArticleHooks from '../hooks/ArticleHooks';
 import {AppContext} from '../contexts/AppContext';
 import CommentList from '../components/CommentList';
 import CommentForm from '../components/CommentForm';
 
+
 const MyArticleView = (props) => {
-  const {setArticles, setMyArticles, setAllArticles } = useContext(AppContext);
+  const {setArticles, setMyArticles, setAllArticles} = useContext(AppContext);
   const {checkUser} = appHooks();
   const {navigation} = props;
   const {deleteArticle} = ArticleHooks();
@@ -26,25 +27,30 @@ const MyArticleView = (props) => {
       console.log(error);
     });
   }, []);
-    return (
-        <Content>
-          <Text style={styles.title}>{title}</Text>
-          <Image style={styles.image} source={{uri: 'http://media.mw.metropolia.fi/wbma/uploads/' + media.filename}} />
-          {uname.name &&<Text style={styles.desc}>This article is written by {uname.name}</Text>}
-          <Text style ={styles.desc}>{mediaDesc}</Text>
-          <Text style ={styles.bodytext}>{media.description}</Text>
-          <CommentList fid={fileID} />
-          <CommentForm fid={fileID} />
-        </Content>
-    );
+  return (
+    <Content style={styles.container}>
+      <Card style={styles.card}>
+        <CardItem>
+          <Body>
+            <Text style={styles.title}>{title}</Text>
+          </Body>
+        </CardItem>
+        <CardItem>
+          <Body>
+            <Image style={styles.image} source={{uri: 'http://media.mw.metropolia.fi/wbma/uploads/' + media.filename}} />
+          </Body>
+        </CardItem>
+      </Card>
+      <Text style ={styles.bodytext}>{media.description}</Text>
+      <CommentList fid={fileID} />
+      <CommentForm fid={fileID} navigation={navigation} />
+    </Content>
+  );
 };
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: 40,
+    backgroundColor: '#c9d4d7',
   },
   image: {
     borderRadius: 16,
@@ -54,22 +60,31 @@ const styles = StyleSheet.create({
     marginLeft: 10,
     marginRight: 10,
   },
+  card: {
+    marginLeft: 10,
+    marginRight: 10,
+  },
   title: {
-    fontSize: 32,
+    fontSize: 28,
     fontWeight: '600',
-    marginBottom: 30,
+    marginTop: 10,
     textAlign: 'center',
   },
   desc: {
     marginBottom: 12,
-    fontWeight: '500',
+    fontWeight: '400',
     marginLeft: 15,
     marginRight: 15,
   },
   bodytext: {
+    borderStyle: 'solid',
+    borderColor: 'black',
+    borderWidth: 1,
+    backgroundColor: 'white',
     marginLeft: 10,
     marginRight: 10,
     fontSize: 15,
+    padding: 5,
   },
 });
 
